a05124762ef6a408f13d5b6ca432bfb7aa95aa36
hawtyzhang <hawtysfu@gmail.com>
2013-10-31 06:09:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
+download_cmd="wget "
+
 if [ ! -d ~/nltk_data/xtag_grammar ]; then
     mkdir -p ~/nltk_data/xtag_grammar
 fi
 cd ~/nltk_data/xtag_grammar
+if ! type "wget" > /dev/null; then
+ 	if ! type "curl" > /dev/null; then
+    	echo "Need wget or curl installed."
+    	exit
+	fi
+fi
+if type "curl" > /dev/null; then
+	download_cmd="curl -LOk "
+fi
 if [ ! -d korean ]; then
-	curl -LOk https://github.com/sfu-natlang/xtag-korean-grammar/archive/master.zip
+	cmd=$download_cmd$"https://github.com/sfu-natlang/xtag-korean-grammar/archive/master.zip"
+	eval $cmd
 	unzip master 
 	mv xtag-korean-grammar-master korean
 	rm -f master.zip 
+	
 fi
 if [  ! -d english ]; then
-	curl -LOk https://github.com/sfu-natlang/xtag-english-grammar/archive/master.zip 
+	cmd=$download_cmd$"https://github.com/sfu-natlang/xtag-english-grammar/archive/master.zip"
+	eval $cmd
 	unzip master 
 	mv xtag-english-grammar-master english
-	:rm -f master.zip
+	rm -f master.zip
 fi
-curl -O https://raw.github.com/sfu-natlang/xtag-nltk/master/util.py
-curl -O https://raw.github.com/sfu-natlang/xtag-nltk/master/load.py
-curl -O https://raw.github.com/sfu-natlang/xtag-nltk/master/feature.py
+
+eval $download_cmd$"https://raw.github.com/sfu-natlang/xtag-nltk/master/util.py"
+eval $download_cmd$"https://raw.github.com/sfu-natlang/xtag-nltk/master/load.py"
+eval $download_cmd$"https://raw.github.com/sfu-natlang/xtag-nltk/master/feature.py"
 
 python util.py
 rm -f *.{py,pyc}
